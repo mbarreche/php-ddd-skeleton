5e3d5b5f9aeb130aa43b1861d1ef7d76f290190f
Marcos Antonio Barreche Salguero <mbarreche@freepik.com>
2022-12-10 14:53:21 +0100

    test: create a new test

--- tests/src/Mooc/Users/Infrastructure/UserRepositoryTest.php
+++ tests/src/Mooc/Users/Infrastructure/UserRepositoryTest.php
@@ -2,7 +2,9 @@
 
 namespace CodelyTv\Tests\Mooc\Users\Infrastructure;
 
+use CodelyTv\Mooc\Users\Domain\exceptions\UserNotFoundException;
 use CodelyTv\Mooc\Users\Domain\UserRepository;
+use CodelyTv\Tests\Mooc\Users\Application\UserIdMother;
 use CodelyTv\Tests\Mooc\Users\Application\UserMother;
 use CodelyTv\Tests\Shared\Infrastructure\PhpUnit\InfrastructureTestCase;
 
@@ -17,5 +19,12 @@ class UserRepositoryTest extends InfrastructureTestCase
     public function it_should_save_a_user(): void
     {
         $this->repository()->saveUser(UserMother::random());
+    }
+
+    /** @test */
+    public function it_should_throw_an_exception_if_we_find_a_not_existent_user(): void
+    {
+        $this->expectException(UserNotFoundException::class);
+        $this->repository()->searchById(UserIdMother::random());
     }
 }
